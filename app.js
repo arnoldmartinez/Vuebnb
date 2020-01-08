26a95cea5a45eecf9757e7fb30bf5236724ef905
Arnold Martinez <arnold.martinez.martinez.91@gmail.com>
2020-01-07 20:27:44 -0600

    Create a data property in the vue instance
    Any property of this data object will be available within our template.
    To tell Vue where to bind this data, we can use mustache syntax, that
    is, double curly brackets, for example, {{ myproperty }}. When Vue
    instantiates, it compiles the template, replaces the mustache syntax
    with the appropiate text, and updates the DOM to reflect this. This
    process is called text interpolation

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 var app = new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        title: 'My apartment',
+        address: '12 My Street, My City, My Country',
+        about: 'This is a description of my apartment.'
+    }
 });
