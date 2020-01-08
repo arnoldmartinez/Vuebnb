6bfdbc32b34a6c10829ae903822638fd74a7a33a
Arnold Martinez <arnold.martinez.martinez.91@gmail.com>
2020-01-07 20:54:11 -0600

    Work with some mock data
    While we're developing, it'd be nice to work with some mock data so
    that we can see how our completed page will look.
    
    We've included sample/data.js in the project for this very reason.
    
    Using global variables split over different script files in this way is
    not an ideal practice. We'll only be doing this in the prototype,
    though, and later we'll get this mock-listing sample from the server.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var app = new Vue({
     el: '#app',
     data: {
-        title: 'My apartment',
-        address: '12 My Street, My City, My Country',
-        about: 'This is a description of my apartment.'
+        title: sample.title,
+        address: sample.address,
+        about: sample.about
     }
 });
