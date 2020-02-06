a3008d89aea681a0c61e45ca7722a479f73065eb
Arnold Martinez <arnold.martinez.martinez.91@gmail.com>
2020-02-05 20:25:07 -0600

    Create the blueprint schema

--- database/migrations/2020_02_06_000919_create_listings_table.php
+++ database/migrations/2020_02_06_000919_create_listings_table.php
@@ -14,7 +14,26 @@ class CreateListingsTable extends Migration
     public function up()
     {
         Schema::create('listings', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->primary('id');
+            $table->unsignedInteger('id');
+            $table->string('title');
+            $table->string('address');
+            $table->longText('about');
+
+            // Amenities
+            $table->boolean('amenity_wifi')->default(false);
+            $table->boolean('amenity_pets_allowed')->default(false);
+            $table->boolean('amenity_tv')->default(false);
+            $table->boolean('amenity_kitchen')->default(false);
+            $table->boolean('amenity_breakfast')->default(false);
+            $table->boolean('amenity_laptop')->default(false);
+
+            // Prices
+            $table->string('price_per_night')->nullable();
+            $table->string('price_extra_people')->nullable();
+            $table->string('price_weekly_discount')->nullable();
+            $table->string('price_monthly_discount')->nullable();
+
             $table->timestamps();
         });
     }
