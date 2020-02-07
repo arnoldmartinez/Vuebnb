f5271ba8d0cd52ce71ba0d427b903bf35209b1b9
Arnold Martinez <arnold.martinez.martinez.91@gmail.com>
2020-02-06 22:49:40 -0600

    Move app.js into the Laravel project
    $ cp ../vuebnb/app.js ./resources/assets/js/
    
    We replace the JavaScript dependencies at the bottom of the view
    app.blade.php.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -62,8 +62,6 @@
     </div>
   </div>
 </div>
-<script src="node_modules/vue/dist/vue.js"></script>
-<script src="sample/data.js"></script>
-<script src="app.js"></script>
+<script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
