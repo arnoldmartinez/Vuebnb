22c8c44db4b5ca052da32497d0c9f0d208df4df8
Arnold Martinez <arnold.martinez.martinez.91@gmail.com>
2020-02-06 19:57:29 -0600

    Include links to these new room images
    The client app knows where to find them. Let's add the image paths to
    our listing API payload

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -8,6 +8,12 @@ class ListingController extends Controller
 {
     public function get_listing_api(Listing $listing)
     {
-        return $listing->toJson();
+        $model  = $listing->toArray();
+
+        for($i = 1; $i <=4; $i++) {
+            $model['image_' . $i] = asset('images/' . $listing->id . '/Image_' . $i . '.jpg');
+        }
+
+        return response()->json($model);
     }
 }
