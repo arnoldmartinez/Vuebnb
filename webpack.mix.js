5c1ab0b4dae9a3a865b317d1186e1b1b5280af14
Arnold Martinez <arnold.martinez.martinez.91@gmail.com>
2020-02-19 21:18:16 -0600

    Copy the fonts into the public folder
    We'll use the copy method, which tell Mix to copy the fonts from their
    home directory into the public folder where they can be accessed by the
    frontend app.

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,4 +16,6 @@ mix.js('resources/assets/js/app.js', 'public/js')
         'node_modules/open-sans-all/css/open-sans.css',
         'node_modules/font-awesome/css/font-awesome.css',
         'resources/assets/css/style.css'
-    ], 'public/css/style.css');
+    ], 'public/css/style.css')
+    .copy('node_modules/open-sans-all/fonts', 'public/fonts')
+    .copy('node_modules/font-awesome/fonts', 'public/fonts');
