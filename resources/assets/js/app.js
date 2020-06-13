166b90d2918ab74ddd6554886826f608abf0e1f0
Arnold Martinez <arnold.martinez.martinez.91@gmail.com>
2020-06-13 15:01:06 -0500

    Create store module js
    We need to import Vuex in this file and, like Vue Router, install it
    with Vue.use. This gives special properties to Vue that make it
    compatible with Vuex, such as allowing components to access the store
    via this.$store.
    
    We import the store module in our main app file. and add it to our Vue
    instance.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,10 +1,12 @@
 import "core-js/features/object/assign";
 import Vue from 'vue';
 import router from './router';
+import store from  './store';
 import App from '../components/App.vue';
 
 var app = new Vue({
     el: '#app',
     render: h => h(App),
-    router
+    router,
+    store
 });
