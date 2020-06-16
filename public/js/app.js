7946aec11130ba1aad5a888291d58476facfdafa
Arnold Martinez <arnold.martinez.martinez.91@gmail.com>
2020-06-16 17:59:24 -0500

    Add a toolber link
    We add a link to saved page in the toolbar so that the saved page is
    accessible from any other page. To do this, we'll add an inline ul where
    links are enclosed within a child li.
    
    To display correctly, we'll to add some extra CSS. Firstly, we'll modify
    the #toolbar declaration so that the toolbar uses flex for display.
    We'll also add some new rules below that for displaying the links.

--- public/js/app.js
+++ public/js/app.js
@@ -1921,6 +1921,11 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
   components: {
@@ -4312,6 +4317,18 @@ var render = function() {
             }),
             _vm._v(" "),
             _c("h1", [_vm._v("vuebnb")])
+          ]),
+          _vm._v(" "),
+          _c("ul", { staticClass: "links" }, [
+            _c(
+              "li",
+              [
+                _c("router-link", { attrs: { to: { name: "saved" } } }, [
+                  _vm._v("Saved")
+                ])
+              ],
+              1
+            )
           ])
         ],
         1
